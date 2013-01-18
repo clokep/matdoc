31162532668c508c08824f96280ecf6ece640afb
Patrick Cloke <pcloke@mitre.org>
2013-01-18 08:13:46 -0500

    Use inner functions in matdoc.

--- matdoc.m
+++ matdoc.m
@@ -5,11 +5,11 @@ function matdoc(varargin)
 % @author Garrett Wampole (gwampole@mitre.org)
 % @author Patrick Cloke (pcloke@mitre.org)
 
-    outDir = fullfile('docs', 'matlab');
+    rootOutDir = fullfile('docs', 'matlab');
     
     topics = {};
-    for it = 1:nargin
-        arg = varargin{it};
+    for kt = 1:nargin
+        arg = varargin{kt};
         
         package = meta.package.fromName(arg);
         if (~isempty(package))
@@ -23,14 +23,104 @@ function matdoc(varargin)
     end
     
     processedTopics = {};
-    for it = 1:numel(topics)
-        topic = topics{it};
+    for kt = 1:numel(topics)
+        topic = topics{kt};
 
-        processedTopics = writeDocumentation(topic, outDir, processedTopics);
+        writeDocumentation(topic);
     end
     
     processedTopics = sort(processedTopics);
 %     writeIndex(outFiles);
+
+    function writeDocumentation(topic)
+    % Generates HTML documentation for the given MATLAB topic.
+
+        if (ismember('/', topic))
+            topic = regexprep(topic, '/', '.');
+        elseif (ismember('\\', topic))
+            topic = regexprep(topic, '\\', '.');
+        end
+
+        if (ismember(topic, processedTopics))
+            % This page has already been generated, nothing to do!
+            return;
+        end
+
+        topicLoc = which(topic);
+        if (~isempty(strfind(topicLoc, matlabroot)) || ...
+            ~isempty(strfind(topicLoc, 'built-in')))
+            % This is a built-in MATLAB function.
+            % XXX can we link this to online help docs?
+            return;
+        end
+
+        fprintf('Processing %s\n', topic);
+
+        % Output files should be in folders.
+        outFile = fullfile(rootOutDir, topic2file(topic));
+
+        % XXX use this information to not have links for topics that don't
+        % exist.
+        % hasDocs = ~isempty(topic);
+        html = help2html(topic);
+
+        topicOutDir = fileparts(outFile);
+        if (~exist(topicOutDir, 'dir'))
+            mkdir(topicOutDir);
+        end
+
+        fout = fopen(outFile, 'w');
+        fprintf(fout, '%s', html);
+        fclose(fout);
+
+        processedTopics = [processedTopics, {topic}];
+        writeTopicMembers(outFile, topic);
+    end
+
+    function writeTopicMembers(topicDoc, topic)
+    % Creates documentation for the members of the given MATLAB topic
+    % documentation file.
+    %
+    % @param topicDoc The MATLAB HTML documentation file.
+        % Pattern to match the contents of an href only.
+        hrefPattern = '(?<=<a href=")(.*?)(?=">)';
+        helpwinPattern = 'matlab:helpwin\(''(.*)''\)';
+        helpwinPattern2 = 'matlab:helpwin (.*)';
+
+        d = fileread(topicDoc);
+
+        [matches, splits] = regexp(d, hrefPattern, 'match', 'split');
+
+        fout = fopen(topicDoc, 'w');
+        fprintf(fout, '%s', splits{1});
+
+        for it = 1:numel(matches)
+            href = matches{it};
+            if (~isempty(href))
+                helpwinMatcher = regexp(href, helpwinPattern, 'tokens');
+                helpwinMatcher2 = regexp(href, helpwinPattern2, 'tokens');
+
+                memberName = [];
+
+                if (~isempty(helpwinMatcher))
+                    memberName = helpwinMatcher{1}{1};
+                elseif (~isempty(helpwinMatcher2))
+                    memberName = helpwinMatcher2{1}{1};
+    %             elseif (href.startsWith('matlab:'))
+    %                 elem.remove();
+                end
+
+                if (~isempty(memberName))
+                    writeDocumentation(memberName);
+                    href = fullfile(rootOutDir, topic2url(memberName, topic));
+                end
+            end
+
+            fprintf(fout, '%s%s', href, splits{it + 1});
+        end
+
+        fclose(fout);
+    end
 end
 
 function topics = package2topics(package)
@@ -45,96 +135,6 @@ function topics = package2topics(package)
         functions{it} = [package.Name, '.', functions{it}];
     end
     topics = [topics, functions];
-end
-
-function processedTopics = writeDocumentation(topic, rootOutDir, processedTopics)
-% Generates HTML documentation for the given MATLAB topic.
-
-    if (ismember('/', topic))
-        topic = regexprep(topic, '/', '.');
-    elseif (ismember('\\', topic))
-        topic = regexprep(topic, '\\', '.');
-    end
-    
-    if (ismember(topic, processedTopics))
-        % This page has already been generated, nothing to do!
-        return;
-    end
-    
-    topicLoc = which(topic);
-    if (~isempty(strfind(topicLoc, matlabroot)) || ...
-        ~isempty(strfind(topicLoc, 'built-in')))
-        % This is a built-in MATLAB function.
-        % XXX can we link this to online help docs?
-        return;
-    end
-    
-    fprintf('Processing %s\n', topic);
-
-    % Output files should be in folders.
-    outFile = fullfile(rootOutDir, topic2file(topic));
-
-    % XXX use this information to not have links for topics that don't
-    % exist.
-    % hasDocs = ~isempty(topic);
-    html = help2html(topic);
-    
-    topicOutDir = fileparts(outFile);
-    if (~exist(topicOutDir, 'dir'))
-        mkdir(topicOutDir);
-    end
-    
-    fout = fopen(outFile, 'w');
-    fprintf(fout, '%s', html);
-    fclose(fout);
-
-    processedTopics = [processedTopics, {topic}];
-    processedTopics = writeTopicMembers(outFile, topic, rootOutDir, processedTopics);
-end
-
-function processedTopics = writeTopicMembers(topicDoc, topic, rootOutDir, processedTopics)
-% Creates documentation for the members of the given MATLAB topic
-% documentation file.
-%
-% @param topicDoc The MATLAB HTML documentation file.
-    % Pattern to match the contents of an href only.
-    hrefPattern = '(?<=<a href=")(.*?)(?=">)';
-    helpwinPattern = 'matlab:helpwin\(''(.*)''\)';
-    helpwinPattern2 = 'matlab:helpwin (.*)';
-
-    d = fileread(topicDoc);
-    
-    [matches, splits] = regexp(d, hrefPattern, 'match', 'split');
-
-    fout = fopen(topicDoc, 'w');
-    fprintf(fout, '%s', splits{1});
-
-    for it = 1:numel(matches)
-        href = matches{it};
-        if (~isempty(href))
-            helpwinMatcher = regexp(href, helpwinPattern, 'tokens');
-            helpwinMatcher2 = regexp(href, helpwinPattern2, 'tokens');
-
-            memberName = [];
-
-            if (~isempty(helpwinMatcher))
-                memberName = helpwinMatcher{1}{1};
-            elseif (~isempty(helpwinMatcher2))
-                memberName = helpwinMatcher2{1}{1};
-%             elseif (href.startsWith('matlab:'))
-%                 elem.remove();
-            end
-            
-            if (~isempty(memberName))
-                processedTopics = writeDocumentation(memberName, rootOutDir, processedTopics);
-                href = fullfile(rootOutDir, topic2url(memberName, topic));
-            end
-        end
-        
-        fprintf(fout, '%s%s', href, splits{it + 1});
-    end
-
-    fclose(fout);
 end
 
 function url = topic2url(topic, parentTopic)
